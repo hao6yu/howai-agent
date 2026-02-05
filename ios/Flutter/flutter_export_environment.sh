624bf8a6599a8cf6ab6a49f7fab1672720137124
Hyu <hao6yu@gmail.com>
2026-02-05 02:54:09 -0600

    config changes

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/haoyu/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/haoyu/development/HaoGPT"
+export "FLUTTER_APPLICATION_PATH=/Users/haoyu/development/howai-agent"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
@@ -11,4 +11,4 @@ export "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzUuNw==,RkxVVFRFUl9DSEFOTkVMPXN0YW
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=true"
-export "PACKAGE_CONFIG=/Users/haoyu/development/HaoGPT/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/haoyu/development/howai-agent/.dart_tool/package_config.json"
